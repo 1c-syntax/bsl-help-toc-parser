dbb678f89dab1f7165848c9b4974b39be0addf35
Viktor <zchokobo@gmail.com>
2022-01-20 13:44:19 +0300

    fix: Добавлен таргет

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
 group = "com.github.eightm"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 repositories {
     mavenCentral()
 }
